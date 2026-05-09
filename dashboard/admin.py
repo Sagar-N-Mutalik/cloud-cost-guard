905255733c1cb59d703ee761c6e685c5df263feb
sagar n mutalik <sagarnmutalik6@gmailcom>
2026-05-09 13:49:58 +0530

    fixed git ignore

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import ConnectedAccount, ScanResult
 
-# Register your models here.
+admin.site.register(ConnectedAccount)
+admin.site.register(ScanResult)
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class ConnectedAccount(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    account_name = models.CharField(max_length=100, help_text="e.g., Hackathon Account")
+    iam_role_arn = models.CharField(max_length=255, help_text="The Role ARN the user created for us")
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_scanned = models.DateTimeField(null=True, blank=True)
+    
+    def __str__(self):
+        return f"{self.user.username} - {self.account_name}"
+
+class ScanResult(models.Model):
+    account = models.ForeignKey(ConnectedAccount, on_delete=models.CASCADE)
+    instance_id = models.CharField(max_length=100)
+    hours_running = models.FloatField()
+    scan_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.instance_id} ({self.hours_running} hrs)"
