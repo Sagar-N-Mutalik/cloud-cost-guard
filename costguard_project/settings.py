d9b401e7dc7e11e575891a16f4b4c1e43d8a37b1
sagar n mutalik <sagarnmutalik6@gmailcom>
2026-05-09 19:15:03 +0530

    Prepared for Render deployment with gunicorn and whitenoise

--- costguard_project/settings.py
+++ costguard_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-6=82&i8mrhep1qlwkgad%i=^ythzqdy(6=k+2g5*m0vb(74y1n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,3 +131,6 @@ EMAIL_HOST_PASSWORD = 'uriyejjfyjbgealh' # Replace with a Google App Password
 
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGOUT_REDIRECT_URL = 'login'
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
